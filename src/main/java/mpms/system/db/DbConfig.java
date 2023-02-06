f3fa08183562ab337b5b23eebf7f5d10c319e1bd
xyli <dev12102@linx-info.com>
2023-02-06 09:35:17 +0800

    数据库配置类添加autoClear，5个参数

--- src/main/java/mpms/system/db/DbConfig.java
+++ src/main/java/mpms/system/db/DbConfig.java
@@ -66,4 +66,42 @@ public class DbConfig {
 	public static void autoClear(String tableName, String timeClo, int maxCount, Consumer<Long> consumer) {
 		autoClear(tableName, timeClo, maxCount, null, consumer);
 	}
+
+	/**
+	 * 自动清理数据接口
+	 *
+	 * @param tableName 表名
+	 * @param timeClo   时间字段
+	 * @param maxCount  最大数量
+	 * @param consumer  查询出超过范围的时间回调
+	 */
+	public static void autoClear(String tableName, String timeClo, int maxCount, Consumer<Entity> whereCon, Consumer<Long> consumer) {
+		if (maxCount <= 0) {
+			return;
+		}
+		ThreadUtil.execute(() -> {
+			Entity entity = Entity.create(tableName);
+			if (whereCon != null) {
+				// 条件
+				whereCon.accept(entity);
+			}
+			Page page = new Page(maxCount, 1);
+			page.addOrder(new Order(timeClo, Direction.DESC));
+			PageResult<Entity> pageResult;
+			try {
+				pageResult = Db.use().setWrapper((Character) null).page(entity, page);
+				if (pageResult.isEmpty()) {
+					return;
+				}
+				Entity entity1 = pageResult.get(0);
+				long time = Convert.toLong(entity1.get(timeClo.toUpperCase()), 0L);
+				if (time <= 0) {
+					return;
+				}
+				consumer.accept(time);
+			} catch (SQLException e) {
+				DefaultSystemLog.getLog().error("数据库查询异常", e);
+			}
+		});
+	}
 }
