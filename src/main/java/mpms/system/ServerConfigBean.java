5c4c44a6a884fa413b9536a00c15fc5b54f6c9bf
xyli <dev12102@linx-info.com>
2023-02-07 09:41:47 +0800

    添加ServerConfigBean.java文件

---
+++ src/main/java/mpms/system/ServerConfigBean.java
@@ -0,0 +1,17 @@
+package io.jpom.system;
+
+import cn.hutool.core.io.FileUtil;
+import cn.jiangzeyin.common.spring.SpringUtil;
+import io.jpom.common.BaseServerController;
+import io.jpom.model.data.UserModel;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+
+/**
+ * 配置信息静态变量类
+ */
+@Configuration
+public class ServerConfigBean {
+
+}
