394de3c6f76e72bab0eefb7c92158ffeb750bb2c
xyli <dev12102@linx-info.com>
2023-02-08 10:10:13 +0800

    修改DbSystemMonitorLogService.java文件的包引入

--- src/main/java/mpms/service/dblog/DbSystemMonitorLogService.java
+++ src/main/java/mpms/service/dblog/DbSystemMonitorLogService.java
@@ -1,9 +1,9 @@
-package io.jpom.service.dblog;
+package mpms.service.dblog;
 
 import cn.hutool.db.Entity;
 import cn.hutool.db.PageResult;
-import io.jpom.model.log.SystemMonitorLog;
-import io.jpom.service.h2db.BaseDbCommonService;
+import mpms.model.log.SystemMonitorLog;
+import mpms.service.h2db.BaseDbCommonService;
 import org.springframework.stereotype.Service;
 
 @Service
