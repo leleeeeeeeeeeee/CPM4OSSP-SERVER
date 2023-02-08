76830dd97c77bd00e0907eda5a88adf2e4781c18
xyli <dev12102@linx-info.com>
2023-02-08 09:49:30 +0800

    添加DbMonitorNotifyLogService.java文件

---
+++ src/main/java/mpms/service/dblog/DbMonitorNotifyLogService.java
@@ -0,0 +1,18 @@
+package io.jpom.service.dblog;
+
+import cn.hutool.db.Entity;
+import io.jpom.model.log.MonitorNotifyLog;
+import io.jpom.service.h2db.BaseDbCommonService;
+import io.jpom.system.db.DbConfig;
+import org.springframework.stereotype.Service;
+
+/**
+ * 监控消息
+ *
+ *
+ 
+ */
+@Service
+public class DbMonitorNotifyLogService extends BaseDbCommonService<MonitorNotifyLog> {
+
+}
