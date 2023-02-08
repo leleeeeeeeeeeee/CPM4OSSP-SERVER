ac45ce756d7191801e2ded68015b6fc38af95ed5
xyli <dev12102@linx-info.com>
2023-02-08 10:07:32 +0800

    修改DbSystemMonitorLogService.java文件添加getMonitorData方法

--- src/main/java/mpms/service/dblog/DbSystemMonitorLogService.java
+++ src/main/java/mpms/service/dblog/DbSystemMonitorLogService.java
@@ -13,4 +13,10 @@ public class DbSystemMonitorLogService extends BaseDbCommonService<SystemMonitor
 		super(SystemMonitorLog.TABLE_NAME, "id", SystemMonitorLog.class);
 	}
 
+	public PageResult<SystemMonitorLog> getMonitorData(long startTime, long endTime) {
+		Entity entity = new Entity(SystemMonitorLog.TABLE_NAME);
+		entity.set(" MONITORTIME", ">= " + startTime);
+		entity.set("MONITORTIME", "<= " + endTime);
+		return listPage(entity, null);
+	}
 }
