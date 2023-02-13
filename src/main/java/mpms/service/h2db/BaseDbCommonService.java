dba971f0cacd9f699798d1eeff336e3d6fad38a3
xyli <dev12102@linx-info.com>
2023-02-13 11:26:59 +0800

    修改BaseDbCommonService.java文件，添加insert方法

--- src/main/java/mpms/service/h2db/BaseDbCommonService.java
+++ src/main/java/mpms/service/h2db/BaseDbCommonService.java
@@ -73,4 +73,20 @@ public abstract class BaseDbCommonService<T> {
 	protected String getKey() {
 		return key;
 	}
+
+	/**
+	 * 插入数据
+	 *
+	 * @param t 数据
+	 */
+	public void insert(T t) {
+		Db db = Db.use();
+		db.setWrapper((Character) null);
+		try {
+			Entity entity = this.dataBeanToEntity(t);
+			db.insert(entity);
+		} catch (SQLException e) {
+			throw new LinuxRuntimeException("数据库异常", e);
+		}
+	}
 }
