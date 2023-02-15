a06687986bbe0f6cf555939ae54964db50f96484
xyli <dev12102@linx-info.com>
2023-02-15 16:52:02 +0800

    添加AgentFileService.java文件

---
+++ src/main/java/mpms/service/node/AgentFileService.java
@@ -0,0 +1,11 @@
+package io.jpom.service.node;
+
+import io.jpom.common.BaseOperService;
+import io.jpom.model.AgentFileModel;
+import io.jpom.system.ServerConfigBean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgentFileService  extends BaseOperService<AgentFileModel> {
+
+}
