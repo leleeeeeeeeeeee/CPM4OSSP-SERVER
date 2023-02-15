e82b0bd67aedb297e4241aec6dc85ebde4cd2d3b
xyli <dev12102@linx-info.com>
2023-02-15 09:10:40 +0800

    添加TomcatService.java文件

---
+++ src/main/java/mpms/service/node/tomcat/TomcatService.java
@@ -0,0 +1,27 @@
+package io.jpom.service.node.tomcat;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import io.jpom.common.forward.NodeForward;
+import io.jpom.common.forward.NodeUrl;
+import io.jpom.model.data.NodeModel;
+import io.jpom.permission.BaseDynamicService;
+import io.jpom.plugin.ClassFeature;
+import io.jpom.service.node.NodeService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * tomcat
+ *
+ * 
+ */
+@Service
+public class TomcatService implements BaseDynamicService {
+
+
+}
