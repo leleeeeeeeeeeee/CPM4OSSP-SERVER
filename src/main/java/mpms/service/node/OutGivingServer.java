f3824c37c0127bf149395bc5fe30aa45c520710d
xyli <dev12102@linx-info.com>
2023-02-17 14:44:59 +0800

    修改OutGivingServer.java文件的包引入

--- src/main/java/mpms/service/node/OutGivingServer.java
+++ src/main/java/mpms/service/node/OutGivingServer.java
@@ -1,12 +1,12 @@
-package io.jpom.service.node;
+package mpms.service.node;
 
 import com.alibaba.fastjson.JSONArray;
-import io.jpom.common.BaseOperService;
-import io.jpom.model.data.OutGivingModel;
-import io.jpom.model.data.OutGivingNodeProject;
-import io.jpom.permission.BaseDynamicService;
-import io.jpom.plugin.ClassFeature;
-import io.jpom.system.ServerConfigBean;
+import mpms.common.BaseOperService;
+import mpms.model.data.OutGivingModel;
+import mpms.model.data.OutGivingNodeProject;
+import mpms.permission.BaseDynamicService;
+import mpms.plugin.ClassFeature;
+import mpms.system.ServerConfigBean;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
