663ad29056b94121a2606fe7ad02503e001ff4e3
xyli <dev12102@linx-info.com>
2023-03-03 11:42:46 +0800

    修改测试类yml

---
+++ src/test/java/org/springframework/boot/env/TestYml.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.env;
+
+import org.springframework.core.io.FileSystemResource;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+
+public class TestYml {
+
+
+}
