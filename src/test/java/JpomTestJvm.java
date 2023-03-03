0b15e43ed071b59f075e0f713391a8b1039b945a
xyli <dev12102@linx-info.com>
2023-03-03 11:38:28 +0800

    新增测试类RunFile

---
+++ src/test/java/JpomTestJvm.java
@@ -0,0 +1,11 @@
+import com.sun.tools.attach.AttachNotSupportedException;
+import com.sun.tools.attach.VirtualMachine;
+import com.sun.tools.attach.VirtualMachineDescriptor;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+
+public class JpomTestJvm {
+    
+}
