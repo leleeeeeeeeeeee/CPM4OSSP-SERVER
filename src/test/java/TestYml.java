aa95fd6851eab8fe42f5001186b2ca6ae5c32f02
xyli <dev12102@linx-info.com>
2023-03-03 16:11:56 +0800

    新增测试类TestYml.java

---
+++ src/test/java/TestYml.java
@@ -0,0 +1,15 @@
+import cn.hutool.core.io.resource.ResourceUtil;
+import io.jpom.system.ConfigBean;
+import org.junit.Test;
+import org.springframework.boot.env.YamlPropertySourceLoader;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.FileUrlResource;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+public class TestYml {
+
+	
+}
