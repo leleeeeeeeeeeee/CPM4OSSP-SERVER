4771cfc02752e8399e20d4b4393587c3ac585051
xyli <dev12102@linx-info.com>
2023-03-03 13:52:55 +0800

    新增物理内存获取方法

--- src/test/java/TestCpu.java
+++ src/test/java/TestCpu.java
@@ -9,12 +9,24 @@ import java.lang.management.ManagementFactory;
  */
 public class TestCpu {
     public static void main(String[] args) {
-
+        while (true) {
+            System.out.println(getMemery());
+            System.out.println(getCpuRatio());
+        }
 
 
     }
 
+    public static String getMemery() {
+        OperatingSystemMXBean osmxb = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+        // 总的物理内存+虚拟内存
+        long totalvirtualMemory = osmxb.getTotalSwapSpaceSize();
+        // 剩余的物理内存
+        long freePhysicalMemorySize = osmxb.getFreePhysicalMemorySize();
 
+        Double compare = (1 - freePhysicalMemorySize * 1.0 / totalvirtualMemory) * 100;
+        return "内存已使用:" + compare.intValue() + "%";
+    }
 
 
 
