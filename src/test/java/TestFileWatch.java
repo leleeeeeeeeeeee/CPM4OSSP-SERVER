2ac4d645f08184469e5e6d8018a92931aaba3674
xyli <dev12102@linx-info.com>
2023-03-03 14:09:17 +0800

    新增测试类TestFileWatch

---
+++ src/test/java/TestFileWatch.java
@@ -0,0 +1,14 @@
+import cn.hutool.core.io.watch.WatchMonitor;
+import cn.hutool.core.io.watch.WatchUtil;
+import cn.hutool.core.io.watch.Watcher;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.WatchEvent;
+
+/**
+ * Created by jiangzeyin on 2018/10/2.
+ */
+public class TestFileWatch {
+
+}
