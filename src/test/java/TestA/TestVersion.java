75f5634497066ffdb64c3e4bfa585c35a16e2e41
xyli <dev12102@linx-info.com>
2023-03-03 13:50:04 +0800

    完成测试类TestVersion

--- src/test/java/TestA/TestVersion.java
+++ src/test/java/TestA/TestVersion.java
@@ -2,6 +2,7 @@ package TestA;
 
 public class TestVersion {
     public static void main(String[] args) {
-       
+        String version = TestVersion.class.getPackage().getImplementationVersion();
+        System.out.println(version);
     }
 }
