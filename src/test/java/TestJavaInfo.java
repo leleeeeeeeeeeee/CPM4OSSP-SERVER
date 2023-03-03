d52065ac9172740da0f59dad3fefcab8bccc11d7
xyli <dev12102@linx-info.com>
2023-03-03 14:38:17 +0800

    完成测试类TestJavaInfo

--- src/test/java/TestJavaInfo.java
+++ src/test/java/TestJavaInfo.java
@@ -10,5 +10,9 @@ import java.io.IOException;
  * Created by jiangzeyin on 2019/3/20.
  */
 public class TestJavaInfo {
-    
+    public static void main(String[] args) throws IOException, AttachNotSupportedException, AgentLoadException, AgentInitializationException, ClassNotFoundException {
+        System.out.println(SystemUtil.getJavaRuntimeInfo().getHomeDir());
+        JvmUtil.importFrom(7292);
+        System.out.println(JvmUtil.importFrom(7292));
+    }
 }
