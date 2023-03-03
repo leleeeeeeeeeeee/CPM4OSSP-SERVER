f188654dc1590b13c1af2cb2915c2f535df09396
xyli <dev12102@linx-info.com>
2023-03-03 14:36:36 +0800

    新增测试类TestJavaInfo

---
+++ src/test/java/TestJavaInfo.java
@@ -0,0 +1,14 @@
+import cn.hutool.system.SystemUtil;
+import io.jpom.util.JvmUtil;
+import com.sun.tools.attach.AgentInitializationException;
+import com.sun.tools.attach.AgentLoadException;
+import com.sun.tools.attach.AttachNotSupportedException;
+
+import java.io.IOException;
+
+/**
+ * Created by jiangzeyin on 2019/3/20.
+ */
+public class TestJavaInfo {
+    
+}
