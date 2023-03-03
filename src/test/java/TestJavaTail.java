76235bfc04d89faa26f267e5a0aebaf3306cc749
xyli <dev12102@linx-info.com>
2023-03-03 14:43:49 +0800

    新增测试类TestJavaTail 文件读取方法

--- src/test/java/TestJavaTail.java
+++ src/test/java/TestJavaTail.java
@@ -13,7 +13,28 @@ import java.nio.file.WatchEvent;
  */
 public class TestJavaTail {
 
+    public static void test() throws IOException {
 
+        File file = new File("D:/ssss/a/tset.log");
+        FileOutputStream fileOutputStream = new FileOutputStream(file, true);
+
+        FileDescriptor descriptor = fileOutputStream.getFD();
+        FileInputStream nfis = new FileInputStream(descriptor);
+        String msg = IoUtil.read(nfis, CharsetUtil.CHARSET_UTF_8);
+        System.out.println(msg);
+        System.out.println("nfis>>>" + nfis.read());
+        FileInputStream sfis = new FileInputStream(descriptor);
+        System.out.println("sfis>>>" + sfis.read());
+        System.out.println("nfis>>>" + nfis.read());
+        nfis.close();
+        try {
+            System.out.println("sfis>>>" + sfis.read());
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("nfis执行异常");
+        }
+        sfis.close();
+    }
 
     public static boolean forceDelete(File file) {
         boolean result = file.delete();
