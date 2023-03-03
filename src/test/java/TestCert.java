6700576f3215405721de27a1dd3036526b7ad740
xyli <dev12102@linx-info.com>
2023-03-03 13:44:49 +0800

    完成测试类TestCert

--- src/test/java/TestCert.java
+++ src/test/java/TestCert.java
@@ -11,6 +11,16 @@ import java.security.PublicKey;
  */
 public class TestCert {
     public static void main(String[] args) {
-        
+        PrivateKey privateKey = PemUtil.readPemPrivateKey(ResourceUtil.getStream("D:\\SystemDocument\\Desktop\\1979263_jpom.keepbx.cn.key"));
+        PublicKey publicKey = PemUtil.readPemPublicKey(ResourceUtil.getStream("D:\\SystemDocument\\Desktop\\1979263_jpom.keepbx.cn.pem"));
+
+        RSA rsa = new RSA(privateKey, publicKey);
+        String str = "你好，Hutool";//测试字符串
+
+        String encryptStr = rsa.encryptBase64(str, KeyType.PublicKey);
+        String decryptStr = rsa.decryptStr(encryptStr, KeyType.PrivateKey);
+        System.out.println(encryptStr);
+        System.out.println(decryptStr);
+        System.out.println(str.equals(decryptStr));
     }
 }
