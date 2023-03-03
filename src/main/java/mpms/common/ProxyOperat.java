601ecfedce131703b0dff2c39cad9e5f7dd9fe78
xyli <dev12102@linx-info.com>
2023-03-03 16:49:58 +0800

    新增ProxyOperat.java 方法

---
+++ src/main/java/mpms/common/ProxyOperat.java
@@ -0,0 +1,31 @@
+package io.jpom.common;
+
+import cn.hutool.core.io.FileUtil;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import io.jpom.dao.NodeinfoDao;
+import io.jpom.entity.Nodeinfo;
+import io.jpom.model.data.NodeModel;
+import io.jpom.util.CommandUtil;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author stone
+ */
+
+@Component
+public class ProxyOperat {
+
+
+
+
+
+
+
+}
