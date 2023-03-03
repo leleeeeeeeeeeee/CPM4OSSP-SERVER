63b7f2e4bc0047d997f05af8f5d0d0de4e687eb6
xyli <dev12102@linx-info.com>
2023-03-03 13:51:01 +0800

    新增测试类TestCpu

---
+++ src/test/java/TestCpu.java
@@ -0,0 +1,23 @@
+import com.sun.management.OperatingSystemMXBean;
+
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.lang.management.ManagementFactory;
+
+/**
+ * Created by jiangzeyin on 2019/3/15.
+ */
+public class TestCpu {
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+
+
+
+
+
+}
