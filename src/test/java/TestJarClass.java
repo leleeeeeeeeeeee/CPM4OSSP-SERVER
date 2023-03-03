229a57b6ce06de914447b3697be7036a25f6b52a
xyli <dev12102@linx-info.com>
2023-03-03 14:28:02 +0800

    新增测试类TestJarClass

---
+++ src/test/java/TestJarClass.java
@@ -0,0 +1,19 @@
+import cn.hutool.Hutool;
+import cn.hutool.core.util.ClassUtil;
+import cn.hutool.core.util.URLUtil;
+import io.jpom.common.JpomManifest;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
+/**
+ * Created by jiangzeyin on 2019/4/13.
+ */
+public class TestJarClass {
+
+
+}
