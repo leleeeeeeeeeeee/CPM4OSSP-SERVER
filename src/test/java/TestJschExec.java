eebbf7de8ec01b335f606b01edb2b64a774fd9c5
xyli <dev12102@linx-info.com>
2023-03-03 14:51:15 +0800

    新增测试类TestJschExec

---
+++ src/test/java/TestJschExec.java
@@ -0,0 +1,33 @@
+import cn.hutool.core.io.IoUtil;
+import cn.hutool.core.thread.SyncFinisher;
+import cn.hutool.core.util.CharsetUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.extra.ssh.ChannelType;
+import cn.hutool.extra.ssh.JschUtil;
+import cn.jiangzeyin.common.DefaultSystemLog;
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+public class TestJschExec {
+
+
+
+
+
+
+
+
+
+
+}
