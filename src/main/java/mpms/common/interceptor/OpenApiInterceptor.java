949c356297399ce0c9e978d2a6c417a668a629c6
xyli <dev12102@linx-info.com>
2023-03-03 17:55:57 +0800

    实现 OpenApiInterceptor preHandle方法

--- src/main/java/mpms/common/interceptor/OpenApiInterceptor.java
+++ src/main/java/mpms/common/interceptor/OpenApiInterceptor.java
@@ -17,6 +17,19 @@ import javax.servlet.http.HttpServletResponse;
 @InterceptorPattens(value = "/api/**")
 public class OpenApiInterceptor extends BaseInterceptor {
 
+    @Override
+    protected boolean preHandle(HttpServletRequest request, HttpServletResponse response, HandlerMethod handlerMethod) throws Exception {
+
+        NotLogin methodAnnotation = handlerMethod.getMethodAnnotation(NotLogin.class);
+        if (methodAnnotation == null) {
+            if (handlerMethod.getBeanType().isAnnotationPresent(NotLogin.class)) {
+                return true;
+            }
+        } else {
+            return true;
+        }
+        return checkOpenApi(request, response);
+    }
 
 
 }
