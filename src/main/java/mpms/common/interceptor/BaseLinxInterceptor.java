f0db3a387166e51082d14f68be81bbdb9d7cc910
xyli <dev12102@linx-info.com>
2023-03-03 17:19:58 +0800

    新增BaseLinxInterceptor 拦截器

---
+++ src/main/java/mpms/common/interceptor/BaseLinxInterceptor.java
@@ -0,0 +1,11 @@
+package io.jpom.common.interceptor;
+
+import cn.jiangzeyin.common.interceptor.BaseInterceptor;
+
+/**
+ * 拦截器
+ */
+public abstract class BaseLinxInterceptor extends BaseInterceptor {
+
+
+}
