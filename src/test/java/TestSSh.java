15d5f6623cebdbca9cf165418e1aa5982adb9319
xyli <dev12102@linx-info.com>
2023-03-03 15:42:16 +0800

    TestSSH.java 新增ssh信息打印方法

--- src/test/java/TestSSh.java
+++ src/test/java/TestSSh.java
@@ -11,5 +11,53 @@ import java.io.*;
 public class TestSSh {
     private static final String charset = "UTF-8"; // 设置编码格式
 
+    public static void main(String[] args) throws Exception {
+        Session session = JschUtil.getSession("39.1.109", 23, "root", "kee");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String command;
+        BufferedReader reader = null;
+        ChannelShell channel = (ChannelShell) JschUtil.createChannel(session, ChannelType.SHELL);
+        channel.setPty(true);
+        channel.connect();
+        InputStream in = channel.getInputStream();
+        ThreadUtil.execute(() -> {
+            IoUtil.readLines(in, CharsetUtil.CHARSET_UTF_8, (LineHandler) System.out::println);
+        });
+        ThreadUtil.execute(() -> {
+            try {
+                IoUtil.readLines(channel.getExtInputStream(), CharsetUtil.CHARSET_UTF_8, (LineHandler) System.out::println);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+
+        OutputStream outputStream = channel.getOutputStream();
+        PrintWriter printWriter = new PrintWriter(outputStream);
+        try {
+            while ((command = br.readLine()) != null) {
+                if ("*".equals(command)) {
+                    command = "0x08";
+                    printWriter.write("0x08".toCharArray());
+                    printWriter.flush();
+                    continue;
+                }
+                printWriter.println(command);
+                printWriter.flush();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            channel.disconnect();
+        }
+    }
+
 
 }
