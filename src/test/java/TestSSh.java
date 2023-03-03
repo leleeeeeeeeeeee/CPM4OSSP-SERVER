5f84b263e96218c3b1db6ad325242ea603cc3593
xyli <dev12102@linx-info.com>
2023-03-03 15:39:14 +0800

    新增TestSSh测试类

---
+++ src/test/java/TestSSh.java
@@ -0,0 +1,15 @@
+import cn.hutool.core.io.IoUtil;
+import cn.hutool.core.io.LineHandler;
+import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.core.util.CharsetUtil;
+import cn.hutool.extra.ssh.ChannelType;
+import cn.hutool.extra.ssh.JschUtil;
+import com.jcraft.jsch.*;
+
+import java.io.*;
+
+public class TestSSh {
+
+
+
+}
