f7bb072a9cb822bff2d788af3e723bc7965250f3
xyli <dev12102@linx-info.com>
2023-03-03 11:34:54 +0800

    优化测试类ssh

--- src/test/java/git/TestSshGit.java
+++ src/test/java/git/TestSshGit.java
@@ -20,7 +20,32 @@ import java.io.*;
 import java.security.PrivateKey;
 
 public class TestSshGit {
-	
+	public static void main(String[] args) throws GitAPIException, IOException {
+		File directory = new File("～/test/gitssh");
+		FileUtil.del(directory);
+		Git.cloneRepository()
+				.setURI("git@gitee.com:keepbx/Jpom-demo-case.git").setProgressMonitor(new TextProgressMonitor(new PrintWriter(System.out)))
+				.setDirectory(directory)
+				.setTransportConfigCallback(new TransportConfigCallback() {
+					@Override
+					public void configure(Transport transport) {
+						SshTransport sshTransport = (SshTransport) transport;
+						sshTransport.setSshSessionFactory(new JschConfigSessionFactory() {
+							@Override
+							protected void configure(OpenSshConfig.Host hc, Session session) {
+								session.setConfig("StrictHostKeyChecking", "no");
+							}
+
+							@Override
+							protected JSch createDefaultJSch(FS fs) throws JSchException {
+								JSch jSch = super.createDefaultJSch(fs);
+								return jSch;
+							}
+						});
+					}
+				})
+				.call();
+	}
 
     @Test
 	public void test1() throws GitAPIException {
