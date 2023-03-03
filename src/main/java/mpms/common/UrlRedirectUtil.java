07d7fead10e96478e17ea3d43bb056f3fdd2be2e
xyli <dev12102@linx-info.com>
2023-03-03 17:08:43 +0800

    新增UrlRedirectUtil url重定向类

---
+++ src/main/java/mpms/common/UrlRedirectUtil.java
@@ -0,0 +1,22 @@
+package io.jpom.common;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.extra.servlet.ServletUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.function.Function;
+
+/**
+ * url 重定向
+ * 配置nginx 代理实现
+ *
+ */
+public class UrlRedirectUtil {
+
+
+
+
+
+
+}
