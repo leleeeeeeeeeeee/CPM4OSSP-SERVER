c0defa892ed58d483fbb12f881d7baaf2a5511bf
xyli <dev12102@linx-info.com>
2023-03-03 14:33:44 +0800

    新增外部jar包地址获取

--- src/test/java/TestJarLoader.java
+++ src/test/java/TestJarLoader.java
@@ -10,5 +10,10 @@ import java.util.jar.JarFile;
 import java.util.zip.ZipEntry;
 
 public class TestJarLoader {
-
+    public static void main(String[] args) throws ClassNotFoundException, IOException {
+        JarFile jarFile1 = new JarFile(new File("D:\\Idea\\Jpom\\modules\\agent\\target\\agent-2.4.2.jar"));
+        ZipEntry zipEntry = jarFile1.getEntry("BOOT-INF/classes/cn/keepbx/jpom/common/interceptor/NotAuthorize.class");
+        System.out.println(zipEntry);
+        String path = "D:\\Idea\\Jpom\\modules\\agent\\target\\agent-2.4.2.jar";//外部jar包的路径
+       
 }
