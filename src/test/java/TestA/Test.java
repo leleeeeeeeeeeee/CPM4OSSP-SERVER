c22453c08d010b3a7818e05e181cbefc3701bfd5
xyli <dev12102@linx-info.com>
2023-03-03 13:38:27 +0800

    新建测试类Test

---
+++ src/test/java/TestA/Test.java
@@ -0,0 +1,18 @@
+package TestA;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.ClassUtil;
+import io.jpom.service.user.RoleService;
+import io.jpom.permission.CacheControllerFeature;
+import io.jpom.plugin.ClassFeature;
+import io.jpom.plugin.MethodFeature;
+
+import java.util.Map;
+import java.util.Set;
+
+public class Test {
+    public static void main(String[] args) {
+
+       
+    }
+}
